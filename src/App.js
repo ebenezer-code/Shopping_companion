a7203c9bd28fd993f89d97db78a3bd3b218c111f
Ebenezer Noble <ebenezernoblest@gmail.com>
2023-06-28 09:39:07 +0100

    Made imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import {useState, useReducer, useEffect, useRef} from "react";
+import Alert from "./Components/Alert"
+import List from "./Components/List"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      
     </div>
   );
 }
