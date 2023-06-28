ee6196ee42701f17d99b7aa5ef184718e10e1f6a
Ebenezer Noble <ebenezernoblest@gmail.com>
2023-06-28 11:45:26 +0100

    Added the ref function to the input tag

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
         {state.isModalOpen && <Alert /> }
         <h3>Grocery bud</h3>
         <div className="input-div">
-          <input type="text" className="grocery" placeholder="Enter grocery list" value={inputText} onChange={(e) => setInputText(e.target.value)}/>
+          <input type="text" className="grocery" placeholder="Enter grocery list" value={inputText} onChange={(e) => setInputText(e.target.value)} ref={refContainer}/>
           <button type="submit" className = {state.isEditing ? "grocery-btn-edit" : "grocery-btn"}>
             {state.isEditing ? "Edit" : "Submit"}  
           </button>
